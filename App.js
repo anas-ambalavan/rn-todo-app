a4e3bbc6a4e88826bdbae89e080f3f169148929f
anas <17gcs23@meaec.edu.in>
2021-10-29 08:55:14 +0530

    authScreen added

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-} from "react-native";
-import AddTodoItem from "./components/AddTodoItem";
-import HeaderTab from "./components/HeaderTab";
-import TodoListItem from "./components/TodoListItem";
+import AuthScreen from "./screens/AuthScreen";
+
 import TodoScreen from "./screens/TodoScreen";
 
 export default function App() {
-  return <TodoScreen />;
+  return <AuthScreen />;
 }
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({});
