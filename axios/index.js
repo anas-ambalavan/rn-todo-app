8f7870528fe0894eb65835ac89071ae6de53e591
Anas <17gcs23@meaec.edu.in>
2022-12-14 23:57:21 +0530

    Expo SDK updated, API updated

--- axios/index.js
+++ axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://rn-todo-app-api.herokuapp.com/api",
+  baseURL: "https://todo-app.techzia.in/api",
   // baseURL: "http://localhost:5000/api",
 });
 
